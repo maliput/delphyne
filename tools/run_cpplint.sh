cc6d9681b7f733fb83f42491f129bd90060980d5
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2017-11-16 11:33:46 -0300

    Add headers extension list to cpplint

--- tools/run_cpplint.sh
+++ tools/run_cpplint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 find . -not \( -path ./bridge/drake -prune \) -not \( -path ./bridge/protobuf -prune \) -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' | while read file ; do
-    python tools/cpplint.py --extensions=cc,cpp,c,hh,hpp,h "$file"
+    python tools/cpplint.py --extensions=cc,cpp,c,hh,hpp,h --headers=h,hh,hpp "$file"
 done

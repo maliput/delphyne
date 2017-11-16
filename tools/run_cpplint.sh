7b105d912c66520053e3874a4b4bdcb84db6b983
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2017-11-16 14:34:34 -0300

    Pass all files at once to cpplint to make it run faster

--- tools/run_cpplint.sh
+++ tools/run_cpplint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-find . -not \( -path ./bridge/drake -prune \) -not \( -path ./bridge/protobuf -prune \) -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' | while read file ; do
-    python tools/cpplint.py --extensions=cc,cpp,c,hh,hpp,h --headers=h,hh,hpp "$file"
-done
+python tools/cpplint.py --extensions=c,cc,cpp,cxx,c++,C,h,hh,hpp,hxx,inc --headers=h,hh,hpp,hxx --recursive $( find . -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' )

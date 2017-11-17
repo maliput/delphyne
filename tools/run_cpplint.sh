5f01c6dc94ad11a1f007d9261940d8cdf6bf4088
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2017-11-17 14:14:17 -0300

    Added `--quiet` argument to cpplint script

--- tools/run_cpplint.sh
+++ tools/run_cpplint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python tools/cpplint.py --extensions=c,cc,cpp,cxx,c++,C,h,hh,hpp,hxx,inc --headers=h,hh,hpp,hxx --recursive $( find . -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' )
+python tools/cpplint.py \
+    --quiet \
+    --recursive \
+    --extensions=c,cc,cpp,cxx,c++,C,h,hh,hpp,hxx,inc \
+    --headers=h,hh,hpp,hxx \
+    $( find . -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' )

706cbe4ad2d6216908b32c17bf18f2020934fc29
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2017-11-17 15:39:39 -0300

    Updated run_cpplint.sh with more strict types

--- tools/run_cpplint.sh
+++ tools/run_cpplint.sh
@@ -3,6 +3,5 @@
 python tools/cpplint.py \
     --quiet \
     --recursive \
-    --extensions=c,cc,cpp,cxx,c++,C,h,hh,hpp,hxx,inc \
-    --headers=h,hh,hpp,hxx \
-    $( find . -iname '*.cc' -o -iname '*.cpp' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.hh' -o -iname '*.h' )
+    --extensions=cc,h \
+    $( find . -iname '*.cc' -o -iname '*.h' )

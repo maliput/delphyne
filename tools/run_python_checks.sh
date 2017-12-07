046a21508c9dfb887a3c2b3bcdc0fa3171fead84
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2017-12-07 10:56:51 -0300

    Added custom pylint config file

--- tools/run_python_checks.sh
+++ tools/run_python_checks.sh
@@ -39,8 +39,8 @@ find ./bridge -name \*.py -exec pycodestyle {} + || PEP8FAILED+=1
 
  if [ "$PEP8FAILED" -eq "0" ]; then
   # Run pylint
-  find ./backend -name \*.py -exec pylint --reports=n {} +
-  find ./bridge -name \*.py -exec pylint --reports=n {} +
+  find ./backend -name \*.py -exec pylint {} +
+  find ./bridge -name \*.py -exec pylint {} +
 else
    echo $'\n*** PEP8 failed, not doing pylint ***'
 fi

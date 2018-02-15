bc5344675ecfdeed2960cb0c28d62bae6ac21500
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-15 18:13:07 -0300

    Added ign Model_V to LCM viewer_draw translation.

--- backend/ign_to_lcm_translation.cc
+++ backend/ign_to_lcm_translation.cc
@@ -31,6 +31,8 @@
 
 #include "backend/ign_to_lcm_translation.h"
 #include "drake/lcmt_driving_command_t.hpp"
+#include <drake/lcmt_viewer_draw.hpp>
+#include <ignition/msgs.hh>
 #include "protobuf/automotive_driving_command.pb.h"
 
 #include "backend/system.h"
@@ -38,11 +40,27 @@
 namespace delphyne {
 namespace backend {
 
+int64_t millisFromSecs(int64_t secs) { return secs * 1000; }
+
+int64_t millisFromNsecs(int64_t nsecs) { return nsecs / 1000000; }
+
+std::vector<float> ignToVector(const ignition::msgs::Vector3d& position) {
+  return {static_cast<float>(position.x()), static_cast<float>(position.y()),
+          static_cast<float>(position.z())};
+}
+
+std::vector<float> ignToVector(const ignition::msgs::Quaternion& orientation) {
+  return {
+      static_cast<float>(orientation.w()), static_cast<float>(orientation.x()),
+      static_cast<float>(orientation.y()), static_cast<float>(orientation.z())};
+}
+
 void ignToLcm(const ignition::msgs::AutomotiveDrivingCommand& ignDrivingCommand,
               drake::lcmt_driving_command_t* lcmDrivingCommand) {
   if (ignDrivingCommand.has_time()) {
-    lcmDrivingCommand->timestamp = ignDrivingCommand.time().sec() * 1000 +
-                                   ignDrivingCommand.time().nsec() / 1000000;
+    lcmDrivingCommand->timestamp =
+        millisFromSecs(ignDrivingCommand.time().sec()) +
+        millisFromNsecs(ignDrivingCommand.time().nsec());
   } else {
     int64_t milliseconds = std::chrono::system_clock::now().time_since_epoch() /
                            std::chrono::milliseconds(1);
@@ -54,9 +72,28 @@ void ignToLcm(const ignition::msgs::AutomotiveDrivingCommand& ignDrivingCommand,
 
 void ignToLcm(const ignition::msgs::Model_V& robotModels,
               drake::lcmt_viewer_draw* robotDrawData) {
-  // No-op, since it is not being currently used.
-  // TODO(basicNew) actually implement this.
-  DELPHYNE_ABORT();
+  robotDrawData->timestamp =
+      millisFromSecs(robotModels.header().stamp().sec()) +
+      millisFromNsecs(robotModels.header().stamp().nsec());
+
+  // Each ignition model has many links using the same id, but this is
+  // flattened in LCM
+  robotDrawData->num_links = 0;
+  for (int i = 0; i < robotModels.models_size(); ++i) {
+    auto robotModel = robotModels.models(i);
+
+    for (int j = 0; j < robotModel.link_size(); ++j) {
+      robotDrawData->num_links += 1;
+      auto link = robotModel.link(j);
+      auto pose = link.pose();
+
+      robotDrawData->robot_num.push_back(robotModel.id());
+      robotDrawData->link_name.push_back(link.name());
+
+      robotDrawData->position.push_back(ignToVector(pose.position()));
+      robotDrawData->quaternion.push_back(ignToVector(pose.orientation()));
+    }
+  }
 }
 
 }  // namespace backend

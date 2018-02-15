b67f48837c23bf89d98489940784e26d1234e451
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-15 18:14:06 -0300

    Added tests for ign Model_V to LCM viewer_draw translation.

--- backend/test/ign_to_lcm_translation_test.cc
+++ backend/test/ign_to_lcm_translation_test.cc
@@ -32,6 +32,8 @@
 
 #include "backend/ign_to_lcm_translation.h"
 #include "drake/lcmt_driving_command_t.hpp"
+#include <drake/lcmt_viewer_draw.hpp>
+#include <ignition/msgs.hh>
 #include "protobuf/automotive_driving_command.pb.h"
 
 namespace delphyne {
@@ -75,6 +77,145 @@ GTEST_TEST(ignToLcm, TestAutomotiveDrivingCommandTranslationDefaultValues) {
   // Verify generated LCM message
   EXPECT_EQ(lcmDrivingMsg.steering_angle, 0);
   EXPECT_EQ(lcmDrivingMsg.acceleration, 0);
+}
+
+//////////////////////////////////////////////////
+/// \brief Test that the ignition Robot Model message is properly
+/// translated to its LCM counterpart
+GTEST_TEST(ignToLcm, TestRobotModelTranslation) {
+  ignition::msgs::Model_V robotModels;
+
+  robotModels.mutable_header()->mutable_stamp()->set_sec(123);
+  robotModels.mutable_header()->mutable_stamp()->set_nsec(456000000);
+
+  for (int i = 0; i < 3; ++i) {
+    auto model = robotModels.add_models();
+    model->set_id(i);
+
+    for (int j = 0; j < 2; ++j) {
+      auto link = model->add_link();
+
+      auto pose = link->mutable_pose();
+      auto position = pose->mutable_position();
+      auto orientation = pose->mutable_orientation();
+
+      link->set_name(std::to_string(i) + std::to_string(j));
+
+      position->set_x(i);
+      position->set_y(j);
+      position->set_z(i);
+
+      orientation->set_w(i);
+      orientation->set_x(j);
+      orientation->set_y(i);
+      orientation->set_z(j);
+    }
+  }
+
+  drake::lcmt_viewer_draw robotDrawData;
+  ignToLcm(robotModels, &robotDrawData);
+
+  EXPECT_EQ(robotDrawData.timestamp, 123456);
+
+  EXPECT_EQ(robotDrawData.num_links, 6);
+  EXPECT_EQ(robotDrawData.robot_num.size(), 6);
+  EXPECT_EQ(robotDrawData.link_name.size(), 6);
+  EXPECT_EQ(robotDrawData.position.size(), 6);
+  EXPECT_EQ(robotDrawData.quaternion.size(), 6);
+
+  // Model 0 link 0
+  EXPECT_EQ(robotDrawData.robot_num[0], 0);
+  EXPECT_EQ(robotDrawData.link_name[0], "00");
+
+  EXPECT_EQ(robotDrawData.position[0][0], 0);
+  EXPECT_EQ(robotDrawData.position[0][1], 0);
+  EXPECT_EQ(robotDrawData.position[0][2], 0);
+
+  EXPECT_EQ(robotDrawData.quaternion[0][0], 0);
+  EXPECT_EQ(robotDrawData.quaternion[0][1], 0);
+  EXPECT_EQ(robotDrawData.quaternion[0][2], 0);
+  EXPECT_EQ(robotDrawData.quaternion[0][3], 0);
+
+  // Model 0 link 1
+  EXPECT_EQ(robotDrawData.robot_num[1], 0);
+  EXPECT_EQ(robotDrawData.link_name[1], "01");
+
+  EXPECT_EQ(robotDrawData.position[1][0], 0);
+  EXPECT_EQ(robotDrawData.position[1][1], 1);
+  EXPECT_EQ(robotDrawData.position[1][2], 0);
+
+  EXPECT_EQ(robotDrawData.quaternion[1][0], 0);
+  EXPECT_EQ(robotDrawData.quaternion[1][1], 1);
+  EXPECT_EQ(robotDrawData.quaternion[1][2], 0);
+  EXPECT_EQ(robotDrawData.quaternion[1][3], 1);
+
+  // Model 1 link 0
+  EXPECT_EQ(robotDrawData.robot_num[2], 1);
+  EXPECT_EQ(robotDrawData.link_name[2], "10");
+
+  EXPECT_EQ(robotDrawData.position[2][0], 1);
+  EXPECT_EQ(robotDrawData.position[2][1], 0);
+  EXPECT_EQ(robotDrawData.position[2][2], 1);
+
+  EXPECT_EQ(robotDrawData.quaternion[2][0], 1);
+  EXPECT_EQ(robotDrawData.quaternion[2][1], 0);
+  EXPECT_EQ(robotDrawData.quaternion[2][2], 1);
+  EXPECT_EQ(robotDrawData.quaternion[2][3], 0);
+
+  // Model 1 link 1
+  EXPECT_EQ(robotDrawData.robot_num[3], 1);
+  EXPECT_EQ(robotDrawData.link_name[3], "11");
+
+  EXPECT_EQ(robotDrawData.position[3][0], 1);
+  EXPECT_EQ(robotDrawData.position[3][1], 1);
+  EXPECT_EQ(robotDrawData.position[3][2], 1);
+
+  EXPECT_EQ(robotDrawData.quaternion[3][0], 1);
+  EXPECT_EQ(robotDrawData.quaternion[3][1], 1);
+  EXPECT_EQ(robotDrawData.quaternion[3][2], 1);
+  EXPECT_EQ(robotDrawData.quaternion[3][3], 1);
+
+  // Model 2 link 0
+  EXPECT_EQ(robotDrawData.robot_num[4], 2);
+  EXPECT_EQ(robotDrawData.link_name[4], "20");
+
+  EXPECT_EQ(robotDrawData.position[4][0], 2);
+  EXPECT_EQ(robotDrawData.position[4][1], 0);
+  EXPECT_EQ(robotDrawData.position[4][2], 2);
+
+  EXPECT_EQ(robotDrawData.quaternion[4][0], 2);
+  EXPECT_EQ(robotDrawData.quaternion[4][1], 0);
+  EXPECT_EQ(robotDrawData.quaternion[4][2], 2);
+  EXPECT_EQ(robotDrawData.quaternion[4][3], 0);
+
+  // Model 2 link 1
+  EXPECT_EQ(robotDrawData.robot_num[5], 2);
+  EXPECT_EQ(robotDrawData.link_name[5], "21");
+
+  EXPECT_EQ(robotDrawData.position[5][0], 2);
+  EXPECT_EQ(robotDrawData.position[5][1], 1);
+  EXPECT_EQ(robotDrawData.position[5][2], 2);
+
+  EXPECT_EQ(robotDrawData.quaternion[5][0], 2);
+  EXPECT_EQ(robotDrawData.quaternion[5][1], 1);
+  EXPECT_EQ(robotDrawData.quaternion[5][2], 2);
+  EXPECT_EQ(robotDrawData.quaternion[5][3], 1);
+}
+
+//////////////////////////////////////////////////
+/// \brief Test that the ignition Robot Model message is properly
+/// translated to its LCM counterpart
+GTEST_TEST(ignToLcm, TestRobotModelTranslationDefaultValues) {
+  ignition::msgs::Model_V robotModels;
+  drake::lcmt_viewer_draw robotDrawData;
+
+  ignToLcm(robotModels, &robotDrawData);
+
+  EXPECT_EQ(robotDrawData.num_links, 0);
+  EXPECT_EQ(robotDrawData.robot_num.size(), 0);
+  EXPECT_EQ(robotDrawData.link_name.size(), 0);
+  EXPECT_EQ(robotDrawData.position.size(), 0);
+  EXPECT_EQ(robotDrawData.quaternion.size(), 0);
 }
 
 }  // namespace backend

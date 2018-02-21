28f7e598626f419bdad3bfa7303d6ff4fe1cc99d
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-21 11:10:43 -0300

    Moved last ign_to_lcm test to the proper converter class.

--- backend/test/driving_command_to_ignition_message_converter_test.cc
+++ backend/test/driving_command_to_ignition_message_converter_test.cc
@@ -1,4 +1,4 @@
-// Copyright 2017 Open Source Robotics Foundation
+// Copyright 2018 Open Source Robotics Foundation
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -26,60 +26,37 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include "backend/ign_to_lcm_translation.h"
-
-#include <iostream>
-
-#include "backend/test/helpers.h"
-#include "drake/lcmt_driving_command_t.hpp"
-#include "drake/lcmt_viewer_draw.hpp"
+#include "drake/automotive/gen/driving_command.h"
 #include "gtest/gtest.h"
 #include "ignition/msgs.hh"
-
 #include "protobuf/automotive_driving_command.pb.h"
+
+#include "backend/driving_command_to_ignition_message_converter.h"
 
 namespace delphyne {
 namespace backend {
 
-/////////////////////////////////////////////////
-// \brief Test that the ignition AutomotiveDrivingCommand
-// message is properly translated to its LCM counterpart
-GTEST_TEST(ignToLcm, TestAutomotiveDrivingCommandTranslation) {
-  // Defines the ignition command
+namespace test {
+
+GTEST_TEST(DrivingCommandToIgnitionMessageConverter, TestAutomotiveDrivingCommandTranslation) {
   ignition::msgs::AutomotiveDrivingCommand ign_driving_message;
-  // Defines LCM expected message
-  drake::lcmt_driving_command_t lcm_driving_message;
-  // Fills LCM data
-  ign_driving_message.mutable_time()->set_sec(123);
-  ign_driving_message.mutable_time()->set_nsec(987222111);
+
   ign_driving_message.set_theta(0.12);
   ign_driving_message.set_acceleration(15.7);
 
-  // Translates from ignition to LCM
-  ignToLcm(ign_driving_message, &lcm_driving_message);
+  DrivingCommandToIgnitionMessageConverter converter;
+  std::unique_ptr<BasicVector<double>> output_vector = converter.AllocateDiscreteOutputValue();
 
-  // Verifies generated LCM message
-  EXPECT_EQ(123987, lcm_driving_message.timestamp);
-  EXPECT_EQ(0.12, lcm_driving_message.steering_angle);
-  EXPECT_EQ(15.7, lcm_driving_message.acceleration);
+  converter.ProcessDiscreteOutput(ign_driving_message, output_vector.get());
+
+  auto* const driving_command_vector =
+        dynamic_cast<DrivingCommand<double>*>(output_vector.get());
+
+  EXPECT_EQ(0.12, driving_command_vector->steering_angle());
+  EXPECT_EQ(15.7, driving_command_vector->acceleration());
 }
 
-/////////////////////////////////////////////////
-// \brief Test that the ignition AutomotiveDrivingCommand
-// message is properly translated to its LCM counterpart
-GTEST_TEST(ignToLcm, TestAutomotiveDrivingCommandTranslationDefaultValues) {
-  // Defines the ignition command
-  ignition::msgs::AutomotiveDrivingCommand ign_driving_message;
-  // Defines LCM expected message
-  drake::lcmt_driving_command_t lcm_driving_message;
-
-  // Translates from ignition to LCM
-  ignToLcm(ign_driving_message, &lcm_driving_message);
-
-  // Verifies generated LCM message
-  EXPECT_EQ(lcm_driving_message.steering_angle, 0);
-  EXPECT_EQ(lcm_driving_message.acceleration, 0);
-}
+}  // namespace test
 
 }  // namespace backend
 }  // namespace delphyne

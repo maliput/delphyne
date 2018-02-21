872c88aa670f348c34eaa2b1358ad383768ed144
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-02-21 18:00:30 -0300

    Addressed docstring review comments

--- backend/abstract_value_to_ignition_message_converter.h
+++ backend/abstract_value_to_ignition_message_converter.h
@@ -97,57 +97,87 @@ class AbstractValueToIgnitionMessageConverter
   int get_vector_size() override { return 0; }
 
  protected:
-  // Do the actual conversion from the LCM message to the ignition message.
+
+  // @brief Do the actual conversion from the LCM message to the ignition
+  //        message.
   //
   // @param[in] lcm_message The LCM message retrieved from the input port.
-  //
   // @param[out] ign_message The ignition message, populated with the values
-  // from the LCM messge.
+  //                         from the LCM messge.
   virtual void LcmToIgn(const LCM_TYPE& lcm_message, IGN_TYPE* ign_message) = 0;
 
-  // Do the actual conversion from an ignition message to an LCM message.
+  // @brief Do the actual conversion from an ignition message to an LCM message.
   //
   // @param[in] ign_message The ignition message that we need to convert.
-  //
   // @param[out] lcm_message The LCM message filled with the ign_message values.
   virtual void IgnToLcm(const IGN_TYPE& ign_message, LCM_TYPE* lcm_message) = 0;
 
-  // Helper translation functions to be used by derived classes
-
-  std::vector<float> ignToVector(const ignition::msgs::Vector3d& position) {
+  // @brief Convert an ignition position message to a vector of floats (LCM's
+  //        type for positions).
+  //
+  // @param[in] position The ign position message to convert.
+  //
+  // @return The vector filled with the position values.
+  std::vector<float> ignToVector(const ignition::msgs::Vector3d& position) const {
     return {static_cast<float>(position.x()), static_cast<float>(position.y()),
             static_cast<float>(position.z())};
   }
 
+  // @brief Convert an ignition quaterion message to a vector of floats (LCM's
+  //        type for orientations).
+  //
+  // @param[in] orientation The ign position message to convert.
+  //
+  // @return The vector filled with the position values.
   std::vector<float> ignToVector(
-      const ignition::msgs::Quaternion& orientation) {
+      const ignition::msgs::Quaternion& orientation) const {
     return {static_cast<float>(orientation.w()),
             static_cast<float>(orientation.x()),
             static_cast<float>(orientation.y()),
             static_cast<float>(orientation.z())};
   }
 
-  void lcmToIgn(const float positionData[3],
-                ignition::msgs::Vector3d* positionModel) {
-    positionModel->set_x(positionData[0]);
-    positionModel->set_y(positionData[1]);
-    positionModel->set_z(positionData[2]);
-  }
-
-  void lcmToIgn(const float quaternionData[4],
-                ignition::msgs::Quaternion* quaternionModel) {
-    quaternionModel->set_w(quaternionData[0]);
-    quaternionModel->set_x(quaternionData[1]);
-    quaternionModel->set_y(quaternionData[2]);
-    quaternionModel->set_z(quaternionData[3]);
-  }
-
+  // @brief Converts an ignition time message to an LCM timestamp (which is in
+  //        milliseconds).
+  //
+  // @param[in] ign_time The ignition time message.
+  // @param[out] lcm_timestamp_ms The lcm timestamp (in milliseconds).
   void ignToLcm(const ::ignition::msgs::Time& ign_time,
-                int64_t* lcm_timestamp_ms) {
+                int64_t* lcm_timestamp_ms) const {
     *lcm_timestamp_ms = ign_time.sec() * 1000 + ign_time.nsec() / 1000000;
   }
 
-  void lcmToIgn(int64_t lcm_timestamp_ms, ::ignition::msgs::Time* ign_time) {
+  // @brief Converts an array of floats (LCM's type for positions) to an
+  //        ignition position message.
+  //
+  // @param[in] lcm_position The LCM position array.
+  // @param[out] ign_position The ignition position message.
+  void lcmToIgn(const float lcm_position[3],
+                ignition::msgs::Vector3d* ign_position) const {
+    ign_position->set_x(lcm_position[0]);
+    ign_position->set_y(lcm_position[1]);
+    ign_position->set_z(lcm_position[2]);
+  }
+
+  // @brief Converts an array of floats (LCM's type for quaternions) to an
+  //        ignition quaterion message.
+  //
+  // @param[in] lcm_quaternion The LCM orientation array.
+  // @param[out] ign_quaternion The ign quaternion message.
+  void lcmToIgn(const float lcm_quaternion[4],
+                ignition::msgs::Quaternion* ign_quaternion) const {
+    ign_quaternion->set_w(lcm_quaternion[0]);
+    ign_quaternion->set_x(lcm_quaternion[1]);
+    ign_quaternion->set_y(lcm_quaternion[2]);
+    ign_quaternion->set_z(lcm_quaternion[3]);
+  }
+
+  // @brief Converts an LCM timestamp (in milliseconds) to an ingition time
+  //        message.
+  //
+  // @param[in] lcm_timestamp_ms The LCM timestamp (in milliseconds).
+  // @param[out] ign_time The ignition time message.
+  void lcmToIgn(int64_t lcm_timestamp_ms, ::ignition::msgs::Time* ign_time) const {
     ign_time->set_sec(lcm_timestamp_ms / 1000);
     ign_time->set_nsec(lcm_timestamp_ms % 1000 * 1000000);
   }

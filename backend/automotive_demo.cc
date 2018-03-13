9e92787a4e20e66d9008ec75dd19ded7db768714
Chris Lalancette <clalancette@gmail.com>
2018-03-13 13:16:59 -0400

    Add in the LoadablePriusTrajectoryCar. (#265)
    * Add in the LoadablePriusTrajectoryCar.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Review fixes.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Fix a small style problem.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- backend/automotive_demo.cc
+++ backend/automotive_demo.cc
@@ -22,6 +22,66 @@ std::string MakeChannelName(const std::string& name) {
   return default_prefix + "_" + name;
 }
 
+drake::automotive::Curve2<double> MakeCurve(double radius, double inset) {
+  // TODO(jwnimmer-tri) This function will be rewritten once we have
+  // proper splines.  Don't try too hard to understand it.  Run the
+  // demo to see it first, and only then try to understand the code.
+
+  typedef drake::automotive::Curve2<double>::Point2 Point2d;
+  std::vector<Point2d> waypoints;
+
+  // Start (0, +i).
+  // Straight right to (+r, +i).
+  // Loop around (+i, +r).
+  // Straight back to (+i, 0).
+  waypoints.push_back({0.0, inset});
+  for (int theta_deg = -90; theta_deg <= 180; ++theta_deg) {
+    const Point2d center{radius, radius};
+    const double theta = theta_deg * M_PI / 180.0;
+    const Point2d direction{std::cos(theta), std::sin(theta)};
+    waypoints.push_back(center + (direction * (radius - inset)));
+  }
+  waypoints.push_back({inset, 0.0});
+
+  // Start (+i, 0).
+  // Straight down to (+i, -r).
+  // Loop around (-r, +i).
+  // Straight back to start (implicitly via segment to waypoints[0]).
+  for (int theta_deg = 0; theta_deg >= -270; --theta_deg) {
+    const Point2d center{-radius, -radius};
+    const double theta = theta_deg * M_PI / 180.0;
+    const Point2d direction{std::cos(theta), std::sin(theta)};
+    waypoints.push_back(center + (direction * (radius + inset)));
+  }
+
+  // Many copies.
+  const int kNumCopies = 100;
+  std::vector<Point2d> looped_waypoints;
+  for (int copies = 0; copies < kNumCopies; ++copies) {
+    std::copy(waypoints.begin(), waypoints.end(),
+              std::back_inserter(looped_waypoints));
+  }
+  looped_waypoints.push_back(waypoints.front());
+
+  return drake::automotive::Curve2<double>(looped_waypoints);
+}
+
+std::tuple<drake::automotive::Curve2<double>, double, double>
+CreateTrajectoryParams(int index) {
+  // The possible curves to trace (lanes).
+  static const std::vector<drake::automotive::Curve2<double>> curves{
+      MakeCurve(40.0, 0.0),  // BR
+      MakeCurve(40.0, 4.0),  // BR
+      MakeCurve(40.0, 8.0),
+  };
+
+  // Magic car placement to make a good visual demo.
+  const auto& curve = curves[index % curves.size()];
+  const double start_time = (index / curves.size()) * 0.8;
+  const double kSpeed = 2.0;
+  return std::make_tuple(curve, kSpeed, start_time);
+}
+
 int main(int argc, char* argv[]) {
   // Instantiates a simulator.
   auto simulator =
@@ -38,6 +98,18 @@ int main(int argc, char* argv[]) {
   std::map<std::string, linb::any> simple_params;
   if (simulator->AddLoadableCar("LoadablePriusSimpleCar", simple_params, "1",
                                 &state2) < 0) {
+    return 1;
+  }
+
+  // Add a Loadable Prius Trajectory Car.
+  const auto& params = CreateTrajectoryParams(0);
+  std::map<std::string, linb::any> traj_params;
+  traj_params["curve"] = std::get<0>(params);
+  drake::automotive::TrajectoryCarState<double> state3;
+  state3.set_speed(std::get<1>(params));
+  state3.set_position(std::get<2>(params));
+  if (simulator->AddLoadableCar("LoadablePriusTrajectoryCar", traj_params,
+                                "TrajectoryCar0", &state3) < 0) {
     return 1;
   }
 

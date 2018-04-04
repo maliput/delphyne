018ec314b2f111ca74303c47e651c8e67f0a0031
Chris Lalancette <clalancette@openrobotics.org>
2018-04-04 15:18:39 +0000

    Make the simulation step one at a time.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- backend/python/examples/keyboard_controlled_simulation.py
+++ backend/python/examples/keyboard_controlled_simulation.py
@@ -108,9 +108,8 @@ def demo_callback(runner, launcher, keyboard_handler):
             launcher.terminate()
         elif key == 's':
             if runner.IsPaused():
-                runner.RequestMultiStep(int(SIMULATION_TIME_STEP_SECS * 1000))
-                print("Simulation step of {0}s executed.".
-                      format(SIMULATION_TIME_STEP_SECS))
+                runner.RequestMultiStep(1)
+                print("One simulation step executed.")
             else:
                 print("Simulation step only supported in paused mode.")
 

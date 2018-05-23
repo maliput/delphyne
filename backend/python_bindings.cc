f565c8069f686cdd5ad08ec474888da952847b6c
Daniel Stonier <d.stonier@gmail.com>
2018-05-23 00:17:56 -0400

    dont need the overload_cast

--- backend/python_bindings.cc
+++ backend/python_bindings.cc
@@ -126,10 +126,11 @@ PYBIND11_MODULE(python_bindings, m) {
       .def(py::init(
           [](void) { return std::make_unique<AutomotiveSimulator<double>>(); }))
       .def("Start", &AutomotiveSimulator<double>::Start)
-      .def("AddAgent",
-           py::overload_cast<
-               std::unique_ptr<delphyne::AgentBase<double>>>(
-               &AutomotiveSimulator<double>::AddAgent))
+      .def("AddAgent", &AutomotiveSimulator<double>::AddAgent)
+//      .def("AddAgent",
+//           py::overload_cast<
+//               std::unique_ptr<delphyne::AgentBase<double>>>(
+//               &AutomotiveSimulator<double>::AddAgent))
       .def("AddLoadableAgent",
            py::overload_cast<
                const std::string&, const std::string&,

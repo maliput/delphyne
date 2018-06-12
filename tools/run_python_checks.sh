651820e7d85fa51287dc03431fe127bd75cdaa9d
Alexis Pojomovsky <apojomovsky@ekumenlabs.com>
2018-06-12 10:55:19 -0300

    Check if pylint and pycodestyle are installed

--- tools/run_python_checks.sh
+++ tools/run_python_checks.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+export PATH=$PATH:/home/$USER/.local/bin
+
+check_program_installed() {
+  if ! [ -x "$(command -v $1)" ]; then
+    printf "Error: %s was not found in your system.\n" "$1"
+    printf "Please install it by running:\n\n"
+    printf "pip install --user %s\n\n" "$1"
+    exit 1
+  fi
+}
+
+check_program_installed pycodestyle
+check_program_installed pylint
 
 declare -i PEP8FAILED=0
 declare -i PYLINTFAILED=0
 
-# Run PEP 8
-export PATH=$PATH:/home/$USER/.local/bin
-grep -rl --exclude-dir={tools,utils} '^#!/.*python' . | xargs pycodestyle ||  PEP8FAILED=1
 
+# Run PEP 8
+grep -rl --exclude-dir={tools,utils} '^#!/.*python' . | xargs pycodestyle ||  PEP8FAILED=1
 if [ "$PEP8FAILED" -eq "0" ]; then
   # Run pylint
   grep -rl --exclude-dir={tools,utils} '^#!/.*python' . | xargs pylint || PYLINTFAILED=1

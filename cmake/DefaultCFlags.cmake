bd2755f84c26a35a2322be9136bcf137575f38ad
Chris Lalancette <clalancette@openrobotics.org>
2018-12-07 15:25:35 +0000

    Cleanup the CXX_FLAGS.
    In particular, move them all into DefaultCFlags.cmake, which
    cleans up the top-level quite a bit.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- cmake/DefaultCFlags.cmake
+++ cmake/DefaultCFlags.cmake
@@ -1,31 +1,39 @@
-set (CMAKE_C_FLAGS_RELEASE "")
-set (CMAKE_C_FLAGS_RELEASE "-s")
+#####################################
+# Handle CFlags
 
-set (CMAKE_C_FLAGS_RELEASE " ${CMAKE_C_FLAGS_RELEASE} -O3 -DNDEBUG ${CMAKE_C_FLAGS_ALL}" CACHE INTERNAL "C Flags for release" FORCE)
-set (CMAKE_CXX_FLAGS_RELEASE ${CMAKE_C_FLAGS_RELEASE})
+unset (CMAKE_C_FLAGS_ALL CACHE)
+unset (CMAKE_CXX_FLAGS CACHE)
+
+include (${PROJECT_SOURCE_DIR}/cmake/FindSSE.cmake)
+
+if (SSE2_FOUND)
+  set (CMAKE_C_FLAGS_ALL "-msse -msse2 ${CMAKE_C_FLAGS_ALL}")
+  set (CMAKE_C_FLAGS_ALL "-mfpmath=sse ${CMAKE_C_FLAGS_ALL}")
+endif()
+
+if (SSE3_FOUND)
+  set (CMAKE_C_FLAGS_ALL "-msse3 ${CMAKE_C_FLAGS_ALL}")
+endif()
+if (SSSE3_FOUND)
+  set (CMAKE_C_FLAGS_ALL "-mssse3 ${CMAKE_C_FLAGS_ALL}")
+endif()
+
+if (ENABLE_SSE4)
+  message(STATUS "\nSSE4 will be enabled if system supports it.\n")
+  if (SSE4_1_FOUND)
+    set (CMAKE_C_FLAGS_ALL "-msse4.1 ${CMAKE_C_FLAGS_ALL}")
+  endif()
+  if (SSE4_2_FOUND)
+    set (CMAKE_C_FLAGS_ALL "-msse4.2 ${CMAKE_C_FLAGS_ALL}")
+  endif()
+else()
+  message(STATUS "\nSSE4 disabled.\n")
+endif()
 
 set (CMAKE_C_FLAGS_RELWITHDEBINFO " -g -O2 ${CMAKE_C_FLAGS_ALL}" CACHE INTERNAL "C Flags for release with debug support" FORCE)
 set (CMAKE_CXX_FLAGS_RELWITHDEBINFO ${CMAKE_C_FLAGS_RELWITHDEBINFO})
 
-set (CMAKE_C_FLAGS_DEBUG " -ggdb3 ${CMAKE_C_FLAGS_ALL}" CACHE INTERNAL "C Flags for debug" FORCE)
-set (CMAKE_CXX_FLAGS_DEBUG ${CMAKE_C_FLAGS_DEBUG})
-
-set (CMAKE_C_FLAGS_PROFILE " -fno-omit-frame-pointer -g -pg ${CMAKE_C_FLAGS_ALL}" CACHE INTERNAL "C Flags for profile" FORCE)
-set (CMAKE_CXX_FLAGS_PROFILE ${CMAKE_C_FLAGS_PROFILE})
-
-#####################################
-# Set all the global build flags
-set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}}")
-set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_LINK_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}}")
-set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_LINK_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}}")
-set (CMAKE_MODULE_LINKER_FLAGS "${CMAKE_LINK_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}}")
-
 set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}} -std=c++14")
-
-set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}}")
-set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_LINK_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}}")
-set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_LINK_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}}")
-set (CMAKE_MODULE_LINKER_FLAGS "${CMAKE_LINK_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}}")
 
 # Compiler-specific C++14 activation.
 if ("${CMAKE_CXX_COMPILER_ID} " MATCHES "GNU ")
@@ -39,3 +47,20 @@ elseif ("${CMAKE_CXX_COMPILER_ID} " MATCHES "Clang ")
 else ()
     message(FATAL_ERROR "Your C++ compiler does not support C++14.")
 endif ()
+
+# Check if warning options are avaliable for the compiler and return WARNING_CXX_FLAGS variable
+list(APPEND WARN_LEVEL -Waddress -Warray-bounds -Wcomment -Wformat -Wnonnull)
+list(APPEND WARN_LEVEL -Wparentheses -Wreorder -Wreturn-type)
+list(APPEND WARN_LEVEL -Wsequence-point -Wsign-compare -Wstrict-aliasing)
+list(APPEND WARN_LEVEL -Wstrict-overflow=1 -Wswitch -Wtrigraphs -Wuninitialized)
+list(APPEND WARN_LEVEL -Wunused-function -Wunused-label -Wunused-value)
+list(APPEND WARN_LEVEL -Wunused-variable -Wvolatile-register-var)
+
+# Unable to be filtered flags (failing due to limitations in filter_valid_compiler_warnings)
+set(UNFILTERED_FLAGS "-Wc++14-compat")
+
+filter_valid_compiler_warnings(${WARN_LEVEL} -Wextra -Wno-long-long
+  -Wno-unused-value -Wno-unused-value -Wno-unused-value -Wno-unused-value
+  -Winit-self -Wswitch-default
+  -Wmissing-include-dirs -Wno-pragmas)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}${WARNING_CXX_FLAGS} ${UNFILTERED_FLAGS}")

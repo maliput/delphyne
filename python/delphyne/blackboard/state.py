0a27f5abf8b20c0b5d8523315c9e30c6b1c42ecc
Michel Hidalgo <michel@ekumenlabs.com>
2019-10-22 12:06:13 -0300

    Use py_trees.blackboard.BlackboardClient
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- python/delphyne/blackboard/state.py
+++ python/delphyne/blackboard/state.py
@@ -12,37 +12,37 @@ SIMULATION_KEY = 'simulation'
 
 
 def get_road_geometry():
-    blackboard = py_trees.blackboard.Blackboard()
+    blackboard = py_trees.blackboard.BlackboardClient()
     return blackboard.get(ROAD_GEOMETRY_KEY)
 
 
 def set_road_geometry(road_geometry):
-    blackboard = py_trees.blackboard.Blackboard()
+    blackboard = py_trees.blackboard.BlackboardClient()
     blackboard.set(ROAD_GEOMETRY_KEY, road_geometry, True)
 
 
 def get_simulation():
-    blackboard = py_trees.blackboard.Blackboard()
+    blackboard = py_trees.blackboard.BlackboardClient()
     return blackboard.get(SIMULATION_KEY)
 
 
 def set_simulation(simulation):
-    blackboard = py_trees.blackboard.Blackboard()
+    blackboard = py_trees.blackboard.BlackboardClient()
     blackboard.set(SIMULATION_KEY, simulation, True)
 
 
 def initialize_agent_attributes(agent_name):
-    blackboard = py_trees.blackboard.Blackboard()
+    blackboard = py_trees.blackboard.BlackboardClient()
     blackboard.set(agent_name, {}, True)
 
 
 def get_attribute_for_agent(agent_name, attribute_name):
-    blackboard = py_trees.blackboard.Blackboard()
+    blackboard = py_trees.blackboard.BlackboardClient()
     attributes = blackboard.get(agent_name)
     return attributes.get(attribute_name, None)
 
 
 def set_attribute_for_agent(agent_name, attribute_name, attribute_value):
-    blackboard = py_trees.blackboard.Blackboard()
+    blackboard = py_trees.blackboard.BlackboardClient()
     attributes = blackboard.get(agent_name)
     attributes[attribute_name] = attribute_value

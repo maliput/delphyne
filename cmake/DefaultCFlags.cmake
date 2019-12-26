28395021fb4b46dbaf527eed62896cb04fc0ac14
Franco <53065142+francocipollone@users.noreply.github.com>
2019-12-26 10:13:49 -0300

    Upgrade to c++17.

--- cmake/DefaultCFlags.cmake
+++ cmake/DefaultCFlags.cmake
@@ -33,19 +33,24 @@ endif()
 set (CMAKE_C_FLAGS_RELWITHDEBINFO " -g -O2 ${CMAKE_C_FLAGS_ALL}" CACHE INTERNAL "C Flags for release with debug support" FORCE)
 set (CMAKE_CXX_FLAGS_RELWITHDEBINFO ${CMAKE_C_FLAGS_RELWITHDEBINFO})
 
-set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}} -std=c++14")
+set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE_UPPERCASE}} -std=c++17")
 
-# Compiler-specific C++14 activation.
+# C++ Version
+set (CMAKE_CXX_STANDARD 17)
+set (CMAKE_CXX_STANDARD_REQUIRED ON)
+set (CMAKE_CXX_EXTENSIONS OFF)
+
+# Compiler-specific C++17 activation.
 if ("${CMAKE_CXX_COMPILER_ID} " MATCHES "GNU ")
     execute_process(
         COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
-    if (NOT (GCC_VERSION VERSION_GREATER 5.2))
-        message(FATAL_ERROR "${PROJECT_NAME} requires g++ 5.2 or greater.")
+    if (NOT (GCC_VERSION VERSION_GREATER 6.9))
+        message(FATAL_ERROR "${PROJECT_NAME} requires g++ 7.0 or greater.")
     endif ()
 elseif ("${CMAKE_CXX_COMPILER_ID} " MATCHES "Clang ")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
 else ()
-    message(FATAL_ERROR "Your C++ compiler does not support C++14.")
+    message(FATAL_ERROR "Your C++ compiler does not support C++17.")
 endif ()
 
 # Check if warning options are avaliable for the compiler and return WARNING_CXX_FLAGS variable
@@ -57,7 +62,7 @@ list(APPEND WARN_LEVEL -Wunused-function -Wunused-label -Wunused-value)
 list(APPEND WARN_LEVEL -Wunused-variable -Wvolatile-register-var)
 
 # Unable to be filtered flags (failing due to limitations in filter_valid_compiler_warnings)
-set(UNFILTERED_FLAGS "-Wc++14-compat")
+set(UNFILTERED_FLAGS "-Wc++17-compat")
 
 filter_valid_compiler_warnings(${WARN_LEVEL} -Wextra -Wno-long-long
   -Wno-unused-value -Wno-unused-value -Wno-unused-value -Wno-unused-value

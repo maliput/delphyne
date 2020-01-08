3bc8b3c5b46a89b5c66a39817090af0681eb1c76
Franco Cipollone <franco.c@ekumenlabs.com>
2020-01-08 11:15:28 -0300

    Fix test 59: PriusVisTest

--- src/backend/geometry_utilities.h
+++ src/backend/geometry_utilities.h
@@ -117,7 +117,7 @@ template <typename T>
 drake::lcmt_viewer_load_robot BuildLoadMessage(const drake::geometry::SceneGraph<T>& scene_graph) {
   drake::lcm::DrakeMockLcm lcm;
   drake::lcm::Subscriber<drake::lcmt_viewer_load_robot> sub(&lcm, "DRAKE_VIEWER_LOAD_ROBOT");
-  DispatchLoadMessage(scene_graph, &lcm, drake::geometry::Role::kUnassigned);
+  DispatchLoadMessage(scene_graph, &lcm, drake::geometry::Role::kPerception);
   constexpr int kTimeoutMS{0};
   lcm.HandleSubscriptions(kTimeoutMS);
   return sub.message();

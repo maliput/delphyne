6807db25c6ca685c915d330fbff6377c0a91f0b3
Franco Cipollone <franco.c@ekumenlabs.com>
2022-03-10 09:29:57 -0300

    Implements RuleRailCar agent and RightOfWaySystem.

---
+++ src/agents/rule_rail_car.cc
@@ -0,0 +1,151 @@
+/**
+ * @file src/agents/rule_rail_car.cc
+ *
+ * Copyright 2022 Toyota Research Institute
+ */
+/*****************************************************************************
+ ** Includes
+ *****************************************************************************/
+
+#include "agents/rule_rail_car.h"
+
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+#include <drake/common/eigen_types.h>
+#include <drake/systems/framework/context.h>
+#include <drake/systems/primitives/constant_vector_source.h>
+#include <maliput/api/junction.h>
+#include <maliput/api/lane.h>
+#include <maliput/api/segment.h>
+
+// public headers
+#include "delphyne/macros.h"
+#include "delphyne/roads/find_lane.h"
+#include "gen/rail_follower_params.h"
+#include "gen/rail_follower_state.h"
+
+// private headers
+#include "systems/lane_direction.h"
+#include "systems/rail_follower.h"
+#include "systems/right_of_way_system.h"
+#include "systems/speed_system.h"
+#include "systems/vector_source.h"
+
+/*****************************************************************************
+ ** Namespaces
+ *****************************************************************************/
+
+namespace delphyne {
+
+/*****************************************************************************
+ ** Implementation
+ *****************************************************************************/
+
+RuleRailCarBlueprint::RuleRailCarBlueprint(const std::string& name, const maliput::api::Lane& lane,
+                                           bool direction_of_travel,
+                                           double longitudinal_position,  // s
+                                           double lateral_offset,         // r
+                                           double speed, double nominal_speed)
+    : TypedAgentBlueprint<RuleRailCar>(name),
+      initial_parameters_(lane, direction_of_travel, longitudinal_position, lateral_offset, speed, nominal_speed) {
+  /*********************
+   * Checks
+   *********************/
+  DELPHYNE_VALIDATE(initial_parameters_.lane.segment(), std::invalid_argument,
+                    "The lane to be initialised on is not part of a road segment "
+                    "(subsequently road geometry).");
+  DELPHYNE_VALIDATE(initial_parameters_.lane.segment()->junction(), std::invalid_argument,
+                    "The lane to be initialised on is not connected to a junction "
+                    "(subsequently road geometry).");
+  DELPHYNE_VALIDATE(initial_parameters_.lane.segment()->junction()->road_geometry(), std::invalid_argument,
+                    "The lane to be initialised on is not part of a road geometry.");
+
+  const maliput::api::LanePosition initial_car_lane_position{longitudinal_position, lateral_offset, 0.0};
+  lane.ToInertialPosition(initial_car_lane_position);
+  lane.GetOrientation(initial_car_lane_position);
+}
+
+std::unique_ptr<RuleRailCar> RuleRailCarBlueprint::DoBuildAgentInto(
+    maliput::api::RoadNetwork* road_network, drake::systems::DiagramBuilder<double>* builder) const {
+  DELPHYNE_VALIDATE(road_network != nullptr && road_network->road_geometry() != nullptr, std::invalid_argument,
+                    "Rail cars need a road geometry to drive on, make "
+                    "sure the simulation is built with one.");
+  const maliput::api::RoadGeometry* lane_road_geometry =
+      initial_parameters_.lane.segment()->junction()->road_geometry();
+  DELPHYNE_VALIDATE(lane_road_geometry->id() == road_network->road_geometry()->id(), std::invalid_argument,
+                    "The provided initial lane is not on the same road geometry "
+                    "as that used by the simulation");
+  DELPHYNE_VALIDATE(roads::FindLane(initial_parameters_.lane.id(), *road_network->road_geometry()),
+                    std::invalid_argument,
+                    "The provided initial lane is not within this simulation's "
+                    "RoadGeometry.");
+
+  /******************************************
+   * Initial Context Variables
+   ******************************************/
+  typedef RailFollowerState<double> ContextContinuousState;
+  typedef RailFollowerParams<double> ContextNumericParameters;
+
+  ContextContinuousState context_continuous_state;
+  context_continuous_state.set_s(initial_parameters_.position);
+  context_continuous_state.set_speed(initial_parameters_.speed);
+  ContextNumericParameters context_numeric_parameters;
+  context_numeric_parameters.set_r(initial_parameters_.offset);
+  context_numeric_parameters.set_h(0.0);
+  context_numeric_parameters.set_max_speed(initial_parameters_.nominal_speed);
+
+  /*********************
+   * Instantiate System
+   *********************/
+  AgentBlueprint::DiagramBuilder agent_diagram_builder(this->name());
+
+  // TODO(daniel.stonier): LaneDirection is discombabulating, it is more than
+  // just direction and not even expressive of the lane's direction. Rather
+  // it is intended to be used by the car (not it is not maliput api) to
+  // indicate where in the road network it is and whether it is desired to
+  // travel against the flow the lane's nominal direction (traffic flow).
+  // Probably preferable to not use this at all and specify things separately.
+  LaneDirection lane_direction(&(initial_parameters_.lane), initial_parameters_.direction_of_travel);
+  RailFollower<double>* rail_follower_system = agent_diagram_builder.AddSystem(
+      std::make_unique<RailFollower<double>>(lane_direction, context_continuous_state, context_numeric_parameters));
+  rail_follower_system->set_name(name() + "_system");
+
+  auto speed_setter = agent_diagram_builder.template AddSystem(
+      std::make_unique<delphyne::VectorSource<double>>(initial_parameters_.speed));
+
+  auto row_system = agent_diagram_builder.template AddSystem(std::make_unique<RightOfWaySystem<double>>(road_network));
+
+  SpeedSystem<double>* speed_system = agent_diagram_builder.AddSystem<SpeedSystem>();
+
+  agent_diagram_builder.Connect(speed_setter->output(), row_system->velocity_input());
+
+  agent_diagram_builder.Connect(row_system->velocity_output(), speed_system->command_input());
+
+  agent_diagram_builder.Connect(speed_system->acceleration_output(), rail_follower_system->command_input());
+
+  agent_diagram_builder.Connect(rail_follower_system->velocity_output(), speed_system->feedback_input());
+
+  agent_diagram_builder.Connect(rail_follower_system->lane_state_output(), row_system->lane_state_input());
+
+  agent_diagram_builder.Connect(rail_follower_system->pose_output(), row_system->pose_input());
+
+  /*********************
+   * Diagram Outputs
+   *********************/
+  agent_diagram_builder.ExportStateOutput(rail_follower_system->simple_car_state_output());
+  agent_diagram_builder.ExportPoseOutput(rail_follower_system->pose_output());
+  agent_diagram_builder.ExportVelocityOutput(rail_follower_system->velocity_output());
+
+  Agent::Diagram* agent_diagram = builder->AddSystem(agent_diagram_builder.Build());
+  return std::make_unique<RuleRailCar>(agent_diagram, speed_setter);
+}
+
+RuleRailCar::RuleRailCar(Agent::Diagram* diagram, VectorSource<double>* speed_setter)
+    : Agent(diagram), speed_setter_(speed_setter) {}
+
+void RuleRailCar::SetSpeed(double new_speed_mps) { speed_setter_->Set(new_speed_mps); }
+
+}  // namespace delphyne
---
+++ src/agents/rule_rail_car.h
@@ -0,0 +1,128 @@
+/**
+ * @file src/agents/rule_rail_car.h
+ *
+ * Copyright 2022 Toyota Research Institute
+ */
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include <drake/systems/primitives/constant_vector_source.h>
+#include <maliput/api/lane.h>
+#include <maliput/api/road_network.h>
+
+// public headers
+#include "delphyne/macros.h"
+#include "delphyne/mi6/agent_base.h"
+#include "delphyne/mi6/agent_base_blueprint.h"
+#include "gen/maliput_railcar_state.h"
+#include "systems/speed_system.h"
+#include "systems/vector_source.h"
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace delphyne {
+
+/*****************************************************************************
+** Interfaces
+*****************************************************************************/
+
+/// @brief An agent that follows roads as if they were railroad tracks.
+///
+/// The underlying road network has a reference line for each lane which
+/// is utilised by this agent as a railroad track.
+class RuleRailCar : public Agent {
+ public:
+  DELPHYNE_NO_COPY_NO_MOVE_NO_ASSIGN(RuleRailCar)
+
+  /// Rail car constructor.
+  ///
+  /// @param diagram The Diagram representation of the rail car agent.
+  /// @param speed_setter The speed setting system associated to the
+  ///                     rail car agent.
+  explicit RuleRailCar(Agent::Diagram* diagram, VectorSource<double>* speed_setter);
+
+  /// Sets the speed of this agent.
+  ///
+  /// @param new_speed_mps The new speed for the agent,
+  ///                      in meters per second.
+  void SetSpeed(double new_speed_mps);
+
+ private:
+  // A source to set car speed, in meters per second.
+  VectorSource<double>* speed_setter_;
+};
+
+/// @brief An agent that follows roads as if they were railroad tracks.
+///
+/// The underlying road network has a reference line for each lane which
+/// is utilized by this agent as a railroad track.
+///
+/// Initial position is specified in the lane longitudinal co-ordinate (how far
+/// along the track) and the agent will follow this track exactly - the only
+/// variance it is permitted is the speed with which it follows the track.
+///
+/// Right-of-Way rules are evaluated by the agent, consequently the speed of the agent will toggle
+/// between the desired speed and zero according to the current rule state.
+class RuleRailCarBlueprint : public TypedAgentBlueprint<RuleRailCar> {
+ public:
+  DELPHYNE_NO_COPY_NO_MOVE_NO_ASSIGN(RuleRailCarBlueprint)
+
+  /// Constructs a blueprint to build a RuleRailCar into a simulation.
+  ///
+  /// @param name Unique name for the agent.
+  /// @param lane The lane to start in.
+  /// @param direction_of_travel Designates whether the car will travel
+  ///            with or against the flow specified by the lane's rules
+  /// @param longitudinal_position Initial position on the lane's track
+  ///            (maliput lane coordinate, 's' (m)).
+  /// @param lateral_offset The offset perpendicular to the centre road
+  ///            line (maliput lane coordinate 'r' (m)).
+  /// @param speed The actual initial speed.
+  /// @param nominal_speed The desired cruising speed.
+  explicit RuleRailCarBlueprint(const std::string& name, const maliput::api::Lane& lane, bool direction_of_travel,
+                                double longitudinal_position,  // s
+                                double lateral_offset,         // r
+                                double speed, double nominal_speed);
+
+ private:
+  // Container for the agent's initial configuration.
+  //
+  // Note: this is independent of whatever computational mechanisms
+  // are used internally and is a useful construct for recording and
+  // logging / streaming to debug configuration errors.
+  struct Parameters {
+    const maliput::api::Lane& lane;
+    bool direction_of_travel{true};  // with or against the lane s-axis
+    double position{0.0};            // longitudinal position in lane (m)
+    double offset{0.0};              // lateral position in lane (m)
+    double speed{0.0};               // speed in direction of the lane s-axis (m/s)
+    double nominal_speed{0.0};       // nominal cruising speed (m/s)
+    Parameters(const maliput::api::Lane& lane, bool direction_of_travel,
+               double position,  // s
+               double offset,    // r
+               double speed, double nominal_speed)
+        : lane(lane),
+          direction_of_travel(direction_of_travel),
+          position(position),
+          offset(offset),
+          speed(speed),
+          nominal_speed(nominal_speed) {}
+  } initial_parameters_;
+
+  std::unique_ptr<RuleRailCar> DoBuildAgentInto(maliput::api::RoadNetwork* road_network,
+                                                drake::systems::DiagramBuilder<double>* builder) const override;
+};
+
+/*****************************************************************************
+** Trailers
+*****************************************************************************/
+
+}  // namespace delphyne
